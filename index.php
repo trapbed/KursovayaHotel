0369743db0b737fa5fa4c8e4242672c315c9f9b9
trapbed <148848875+trapbed@users.noreply.github.com>
2024-04-03 10:11:45 +0500

    layout

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>Document</title> -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav id='headerNav'>
+        <img src="images/logo.png" alt="logo" id='logoNav'>
+        <div id="headerNavSpans">
+            <div id="headerNavigation">
+                <a href="">Главная</a>
+                <a href="">Контакты</a>
+                <a href="">Номера</a>
+                <a href="">Услуги</a>
+            </div>
+            <span>Регистрация / Авторизация</span>
+        </div>
+    </nav>
+
+    <main id="general">
+        <div id="emtyGeneralMain"></div>
+        <form id='formGeneralMain' action="" method='POST'>
+            <input type="date" name="" id="">
+            <input type="date" name="" id="">
+            <input type="number" name="" id="">
+            <input type="submit" value="Искать">
+        </form>
+    </main>
+</body>
+</html>
